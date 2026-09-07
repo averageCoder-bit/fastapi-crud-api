18b366b4233fef167f14c23cabd5f08c03c60e2b
averageCoder-bit <kyleeva53@gmail.com>
2026-09-07 11:20:27 +0800

    Stage 3: create with validation

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ app = FastAPI()
 
 tasks = [{"id": 1, "title":"Do some chores", "done":True}, {"id": 2, "title":"Walk the dog", "done":True}, {"id": 3, "title":"Study", "done":False}]
 
-@app.get("/tasks")
-def read_tasks() -> dict:
-    return {"tasks": tasks}
 
-@app.get("/tasks/{id}")
-def get_task(id: int):
-    for task in tasks:
-        if task["id"] == id:
-            return task
-    raise HTTPException(
-        status_code=404,
-        detail=f"Task {id} not found"
-    )
+@app.post("/tasks", status_code=201)
+def create_tasks(title: str):
+    if title == "":
+        raise HTTPException(status_code=400, detail="Title cannot be empty")
+    next_id = max(task["id"] for task in tasks) + 1
+    task = {
+        "id":next_id,
+        "title":title,
+        "done":False
+    }
+    tasks.append(task)
+    return {"Created":task}
+            
+
     
     
 
